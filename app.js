bfc627383b56c793a93a513dff1500b5fe820955
tumana <tumana991@gmail.com>
2022-10-16 23:52:54 +0400

    Create multiplication app

---
+++ app.js
@@ -0,0 +1,36 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const questionEl = document.getElementById("question");
+const inputEl = document.getElementById("input");
+const formEl = document.getElementById("form");
+const scoreEl = document.getElementById("score");
+
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+
+if(!score){
+    score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`;
+
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
+
+const correctAnswer = num1 * num2;
+
+formEl.addEventListener("submit", ()=>{
+    const userAnswer = +inputEl.value;
+    if(userAnswer === correctAnswer){
+        score++;
+        updateLocalStorage()
+    } else {
+        score--;
+        updateLocalStorage()
+    }
+});
+
+function updateLocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score))
+}
